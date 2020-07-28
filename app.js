ce80a42d103e09c53a5c0440992dc449c74421d7
Uskhu <usukhbayarch1211@gmail.com>
2020-07-28 20:03:51 +0800

    commit1

---
+++ app.js
@@ -0,0 +1,11 @@
+var uiController = (function () {})();
+var financeController = (function () {})();
+var appController = (function (uiCtrl, fiCtrl) {
+    function ctrlAddItem(){};
+  document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which===13) {
+        ctrlAddItem();
+    } else 
+  });
+})(uiController, financeController);
